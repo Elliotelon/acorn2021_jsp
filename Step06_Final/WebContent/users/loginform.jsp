982dff2c8ac490cf7db6e404f1b59b1c20292f83
kimminkyu <kmk50009@naver.com>
2021-01-05 12:16:44 +0900

    up24

--- Step06_Final/WebContent/users/loginform.jsp
+++ Step06_Final/WebContent/users/loginform.jsp
@@ -4,26 +4,23 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>/users/signup_form.jsp</title>
+<title>users/loginform.jsp</title>
 </head>
 <body>
 <div class="container">
-	<h1>회원 가입 폼 입니다.</h1>
-	<form action="signup.jsp" method="post">
+	<h1>로그인 폼입니다.</h1>
+	<form action="login.jsp" method="post">
 		<div>
 			<label for="id">아이디</label>
 			<input type="text" name="id" id="id"/>
 		</div>
 		<div>
 			<label for="pwd">비밀번호</label>
-			<input type="password" name="pwd" id="pwd"/>
+			<input type="text" name="pwd" id="pwd"/>
 		</div>
-		<div>
-			<label for="email">이메일</label>
-			<input type="email" name="email" id="email"/>
-		</div>
-		<button type="submit">가입</button>
+		<button type="submit">로그인</button>
 	</form>
 </div>
+
 </body>
 </html>
--- Step06_Final/src/test/users/dao/UsersDao.java
+++ Step06_Final/src/test/users/dao/UsersDao.java
@@ -2,6 +2,7 @@ package test.users.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import test.users.dto.UsersDto;
 import test.util.DbcpBean;
@@ -11,12 +12,51 @@ public class UsersDao {
 	private static UsersDao dao;
 	//생성자
 	private UsersDao() {}
-	//Dao의 참조값을 리턴해주는 static 메소드
+	//Dao 의 참조값을 리턴해주는 static 메소드
 	public static UsersDao getInstance() {
 		if(dao==null) {
 			dao=new UsersDao();
 		}
 		return dao;
+	}
+	//인자로 전달된 정보가 유효한 정보인지 여부를 리턴하는 메소드
+	public boolean isValid(UsersDto dto) {
+		//아이디 비밀번호가 유효한 정보인지 여부를 담을 지역변수 만들고 초기값 부여
+		boolean isValid=false;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = new DbcpBean().getConn();
+			//select문 작성
+			String sql = "SELECT id FROM users WHERE id=? and pwd=?";
+			pstmt = conn.prepareStatement(sql);
+			// ? 에 바인딩 할게 있으면 여기서 바인딩한다.
+			pstmt.setString(1, dto.getId());
+			pstmt.setString(2, dto.getPwd());
+			//select 문 수행하고 ResultSet 받아오기
+			rs = pstmt.executeQuery();
+			//while문 혹은 if문에서  ResultSet으로 부터 data 추출
+			if(rs.next()) {//만일 select 된 row가 있다면
+				//유효한 정보이므로 isValid에 true를 대입한다.
+				isValid=true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		//아이디가 유효한지 여부를 리턴해준다.
+		return isValid;
 	}
 	
 	//회원 정보를 저장하는 메소드
@@ -26,13 +66,16 @@ public class UsersDao {
 		int flag = 0;
 		try {
 			conn = new DbcpBean().getConn();
-			//실행할 insert,update,delete문 구성
-			String sql = "";
+			//실행할 insert, update, delete 문 구성
+			String sql = "INSERT INTO users"
+					+ " (id, pwd, email, regdate)"
+					+ " VALUES(?, ?, ?, SYSDATE)";
 			pstmt = conn.prepareStatement(sql);
-			//?에 바인딩할 내용이 있으면 바인딩한다.
-
-			//sql문 실행하고 변화된 row갯수 리턴받기
-			flag = pstmt.executeUpdate();
+			//? 에 바인딩할 내용이 있으면 바인딩한다.
+			pstmt.setString(1, dto.getId());
+			pstmt.setString(2, dto.getPwd());
+			pstmt.setString(3, dto.getEmail());
+			flag = pstmt.executeUpdate(); //sql 문 실행하고 변화된 row 갯수 리턴 받기
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
