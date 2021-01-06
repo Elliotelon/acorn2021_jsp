573168685e721d1bc9afb5ae0e77fa26b3b2ee66
kimminkyu <kmk50009@naver.com>
2021-01-06 15:52:45 +0900

    up30

--- Step06_Final/WebContent/cafe/detail.jsp
+++ Step06_Final/WebContent/cafe/detail.jsp
@@ -1,24 +1,19 @@
-<%@page import="test.cafe.dto.CafeDto"%>
 <%@page import="test.cafe.dao.CafeDao"%>
+<%@page import="test.cafe.dto.CafeDto"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%
 	//1. GET 방식 파라미터로 전달되는 자세히 보여줄 글 번호를 읽어온다.
 	int num=Integer.parseInt(request.getParameter("num"));
-	
-	//2. 글번호를 이용해서 DB에서 글 정보를 읽어온다.
+	//2. 글번호를 이용해서 DB 에서 글정보를 읽어온다.
 	CafeDto dto=CafeDao.getInstance().getData(num);
 	//3. 응답한다.
-
-	
-
-
-%>
+%>    
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>cafe/detail.jsp</title>
+<title>/cafe/detail.jsp</title>
 </head>
 <body>
 <div class="container">
@@ -50,6 +45,25 @@
 			</td>
 		</tr>
 	</table>
+	<%
+		//session scope 에서 로그인된 아이디를 읽어와 본다. ( null 일수도 있음 )
+		String id=(String)session.getAttribute("id");
+	%>
+	<ul>
+		<li><a href="list.jsp">목록보기</a></li>
+		<%if(dto.getWriter().equals(id)){ %>
+			<li><a href="private/updateform.jsp?num=<%=dto.getNum()%>">수정</a></li>
+			<li><a href="javascript:deleteConfirm()">삭제</a></li>
+		<%} %>
+	</ul>
 </div>
+<script>
+	function deleteConfirm(){
+		let isDelete=confirm("글을 삭제 하시겠습니까?");
+		if(isDelete){
+			location.href="private/delete.jsp?num=<%=dto.getNum()%>";
+		}
+	}
+</script>
 </body>
 </html>
--- Step06_Final/src/test/cafe/dao/CafeDao.java
+++ Step06_Final/src/test/cafe/dao/CafeDao.java
@@ -18,6 +18,75 @@ public class CafeDao {
 		}
 		return dao;
 	}
+	//인자로 전달되는 글내용을 수정반영하는 메소드
+	public boolean update(CafeDto dto) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int flag = 0;
+		try {
+			conn = new DbcpBean().getConn();
+			//실행할 insert,update,delete문 구성
+			String sql = "UPDATE board_cafe"
+					+ " SET title=?, content=?"
+					+ " WHERE num=?";
+			pstmt = conn.prepareStatement(sql);
+			//?에 바인딩할 내용이 있으면 바인딩한다.
+			pstmt.setString(1, dto.getTitle());
+			pstmt.setString(2, dto.getContent());
+			pstmt.setInt(3, dto.getNum());
+			//sql문 실행하고 변화된 row갯수 리턴받기
+			flag = pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		if (flag > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	//인자로 전달되는 글번호를 이용해서 삭제하는 메소드
+	public boolean delete(int num) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int flag = 0;
+		try {
+			conn = new DbcpBean().getConn();
+			//실행할 insert,update,delete문 구성
+			String sql = "DELETE FROM board_cafe"
+					+ " WHERE num=?";
+			pstmt = conn.prepareStatement(sql);
+			//?에 바인딩할 내용이 있으면 바인딩한다.
+			pstmt.setInt(1, num);
+			//sql문 실행하고 변화된 row갯수 리턴받기
+			flag = pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		if (flag > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
 	//인자로 전달되는 글번호에 해당하는 글정보를 리턴하는 메소드
 	public CafeDto getData(int num) {
 		CafeDto dto=null;
